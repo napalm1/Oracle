19f59f237e8107425db1ac894e1fa9e393cabc2c
Mike Botsko <botsko@gmail.com>
2013-08-06 10:32:23 -0700

    Adding table setup code

--- src/main/java/me/botsko/oracle/Oracle.java
+++ src/main/java/me/botsko/oracle/Oracle.java
@@ -2,6 +2,7 @@ package me.botsko.oracle;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
@@ -83,6 +84,9 @@ public class Oracle extends JavaPlugin {
 		}
 
 		if(isEnabled()){
+			
+			// Setup databases
+			setupDatabase();
 		
 //			try {
 //			    Metrics metrics = new Metrics(this);
@@ -220,6 +224,94 @@ public class Oracle extends JavaPlugin {
 			}
 		}
 		return con;
+	}
+	
+	
+	/**
+	 * 
+	 */
+	protected void setupDatabase() {
+
+		try {
+			final Connection conn = dbc();
+			if (conn == null)
+				return;
+			
+			String query = "CREATE TABLE IF NOT EXISTS `oracle_announcements` (" +
+					"`id` int(11) NOT NULL AUTO_INCREMENT," +
+					"`announcement` varchar(255) NOT NULL," +
+					"`type` varchar(16) NOT NULL," +
+					"`is_active` tinyint(1) NOT NULL," +
+					"PRIMARY KEY (`id`)" +
+					") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+			Statement st = conn.createStatement();
+			st.executeUpdate(query);
+
+			query = "CREATE TABLE IF NOT EXISTS `oracle_bans` (" +
+					"`ban_id` int(11) NOT NULL AUTO_INCREMENT," +
+					"`player_id` int(11) NOT NULL," +
+					"`staff_player_id` int(11) NOT NULL," +
+					"`reason` varchar(155) NOT NULL," +
+					"`epoch` int(11) NOT NULL," +
+					"`unbanned` tinyint(1) NOT NULL DEFAULT '0'," +
+					"PRIMARY KEY (`id`)" +
+					") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+			st.executeUpdate(query);
+			
+			query = "CREATE TABLE IF NOT EXISTS `oracle_ips` (" +
+					"`ip_id` int(11) unsigned NOT NULL AUTO_INCREMENT," +
+					"`ip` int(11) unsigned NOT NULL," +
+					"PRIMARY KEY (`ip_id`)" +
+					") ENGINE=InnoDB  DEFAULT CHARSET=latin1;";
+			st.executeUpdate(query);
+			
+			query = "CREATE TABLE IF NOT EXISTS `oracle_joins` (" +
+					"`join_id` int(11) unsigned NOT NULL AUTO_INCREMENT," +
+					"`player_count` smallint(4) unsigned NOT NULL," +
+					"`player_id` int(11) unsigned NOT NULL," +
+					"`player_join` int(11) NOT NULL," +
+					"`player_quit` int(11) unsigned DEFAULT NULL," +
+					"`playtime` int(11) unsigned DEFAULT NULL," +
+					"`ip_id` int(11) unsigned NOT NULL," +
+					"PRIMARY KEY (`join_id`)" +
+					") ENGINE=InnoDB  DEFAULT CHARSET=latin1;";
+			st.executeUpdate(query);
+
+			query = "CREATE TABLE IF NOT EXISTS `oracle_players` (" +
+					"`player_id` int(11) unsigned NOT NULL AUTO_INCREMENT," +
+					"`player` varchar(16) NOT NULL," +
+					"PRIMARY KEY (`player_id`)" +
+					") ENGINE=InnoDB  DEFAULT CHARSET=latin1;";
+			st.executeUpdate(query);
+			
+			query = "CREATE TABLE IF NOT EXISTS `oracle_unbans` (" +
+					"`unban_id` int(11) NOT NULL AUTO_INCREMENT," +
+					"`player_id` int(11) unsigned NOT NULL," +
+					"`staff_player_id` int(11) unsigned NOT NULL," +
+					"`epoch` int(11) unsigned NOT NULL," +
+					"PRIMARY KEY (`id`)" +
+					") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+			st.executeUpdate(query);
+			
+			query = "CREATE TABLE IF NOT EXISTS `oracle_warnings` (" +
+					"`warning_id` int(11) unsigned NOT NULL AUTO_INCREMENT," +
+					"`player_id` int(11) unsigned NOT NULL," +
+					"`reason` varchar(155) NOT NULL," +
+					"`staff_player_id` int(11) unsigned NOT NULL," +
+					"`date_created` int(11) unsigned NOT NULL," +
+					"`deleted` tinyint(1) NOT NULL DEFAULT '0'," +
+					"PRIMARY KEY (`warning_id`)" +
+					") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+			st.executeUpdate(query);
+
+
+			st.close();
+			conn.close();
+			
+		} catch (SQLException e) {
+			log("Database connection error: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 	
