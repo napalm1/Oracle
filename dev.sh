2be0fdbe6295f7d8e7d2f34c5156358fc627a739
Mike Botsko <botsko@gmail.com>
2013-11-10 16:38:51 -0800

    Updating pom/deploy info

--- dev.sh
+++ dev.sh
@@ -3,6 +3,6 @@
 # Build maven
 mvn package
 
-mv target/oracle-nightly.jar /Users/botskonet/Minecraft/TestServer/plugins/oracle-nightly.jar
+mv target/oracle-*.jar /Users/botskonet/Minecraft/TestServer/plugins/oracle-nightly.jar
 
 echo "BUILD COMPLETE"
